ae9d31fd7ad07553449441d9d905d5a1e5167585
Debbie <tkudebby@gmail.com>
2024-03-21 21:08:14 +0800

    完成代辦事項顯示功能

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 # Create your views here.
 def todo(request):
-    return render(request, "todo/todo.html")
+    # todos = Todo.objects.all() 這樣無論誰登入都會看到全部人的資料，應該用filter
+    todos = None
+    if request.user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
+
+    return render(request, "todo/todo.html", {"todos": todos})
